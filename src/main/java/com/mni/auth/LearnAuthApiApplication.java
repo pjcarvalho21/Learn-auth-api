882c16e5a2885984e90a063cac8cc3a26d45e5fd
Paulo Carvalho <pjcarvalho21@gmail.com>
2021-11-22 13:32:19 -0300

    COMMIT INICIAL

---
+++ src/main/java/com/mni/auth/LearnAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.mni.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAuthApiApplication.class, args);
+	}
+
+}
